2be9729629188e44caf22c1a7c73ffc0381a6cf4
Alexander Antoschuk <aantoschuk@codempire.io>
2022-05-17 12:38:07 +0300

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { ACLModule } from './acl/acl.module';
-import { AppService } from './app.service';
+import { DatabaseConfig } from './config/database';
+import { config } from './constants/config';
 
 @Module({
-  imports: [ACLModule],
-  providers: [AppService],
+  imports: [ACLModule, ConfigModule.forRoot({
+    isGlobal: true,
+    load: [config]
+  }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig
+    })
+  ],
 })
 export class AppModule { }
